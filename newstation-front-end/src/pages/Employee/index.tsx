462ac17ca6c5f478daf14db11923d33dd2b39b2c
Dominic Gabriel Lasap <dlasap@dnamicro.com>
2021-06-19 03:32:37 +0800

    fixing UpdateForn

--- newstation-front-end/src/pages/Employee/index.tsx
+++ newstation-front-end/src/pages/Employee/index.tsx
@@ -17,6 +17,40 @@ const Employee = () => {
 
   const [open, setOpen] = useState<boolean>(true);
   const [employees, setEmployees] = useState<string[][]>([])
+  const [showEdit, setShowEdit] = useState<boolean>(false);
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+  const [error, setError] = useState<string[]>([]);
+  const [errorRegister, setErrorRegister] = useState<boolean>(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState<boolean>(false);
+  const [editedEmployee, setEditedEmployee] = useState<State>({
+    id: "",
+    company_id: "",
+    employee_first_name: "",
+    employee_middle_name: "",
+    employee_last_name: "",
+    employee_email_address: "",
+    employee_password: "",
+    employee_confirm_password: "",
+    employee_address: "",
+    employee_position: "",
+    employee_contact_number: "",
+    employee_image: "",
+    employee_status: "Active",
+    created_at: "",
+    updated_at: ""
+  })
+
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+  };
+
+  const handleClickShowConfirmPassword = () => {
+    setShowConfirmPassword(!showConfirmPassword);
+  };
 
   const handleLogoutButton = () => {
     logout();
@@ -33,8 +67,24 @@ const Employee = () => {
   };
 
   const handleEditButton = (id: string) => {
+    const {
+      employee_password, employee_email_address, employee_first_name, employee_middle_name, employee_last_name,
+      employee_contact_number, employee_address, employee_position
+    } = editedEmployee;
     console.log('editing.............')
-    // const result = axios.delete('/employee/' + id);
+
+    const result = axios.put('/employee/' + id, {
+      employee_email_address: employee_email_address.toLowerCase(),
+      employee_password: employee_password,
+      employee_first_name: employee_first_name,
+      employee_middle_name: employee_middle_name,
+      employee_last_name: employee_last_name,
+      employee_position: employee_position,
+      employee_contact_number: employee_contact_number,
+      employee_address: employee_address
+    });
+    console.log(result);
+    setShowEdit(true);
   }
 
   const handleDeleteButton = async (id: string) => {
@@ -92,6 +142,7 @@ const Employee = () => {
       <Navbar open={open} handleDrawerOpen={handleDrawerOpen} handleLogoutButton={handleLogoutButton} />
       <Sidenav open={open} handleDrawerClose={handleDrawerClose} />
       <EmployeeTable employees={employees} />
+      {showEdit ? (<h1>Hello!</h1>) : ""}
     </div>
   )
 }
